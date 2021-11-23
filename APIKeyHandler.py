7057ad68f1ad3d0233f73e89ce299daa3ff40b4c
Peter Strong <peterstrong8343@gmail.com>
2021-11-23 18:00:12 -0500

    Update

---
+++ APIKeyHandler.py
@@ -0,0 +1,4 @@
+# Foursquare API Key
+
+def get_key():
+    return "ENTER KEY HERE"
---
+++ getLocations.py
@@ -0,0 +1,28 @@
+from APIKeyHandler import get_key
+import pprint
+import pgeocode
+import requests
+
+# Convert zip code to latitude longitude
+nomi = pgeocode.Nominatim('us')
+query = nomi.query_postal_code("18031")
+lat = str(query["latitude"])
+long = str(query["longitude"])
+
+llString = str(lat) + '%2C' + str(long)
+radius = '10000'
+category = 'arcade'
+
+url = "https://api.foursquare.com/v3/places/search?query="+category+"&ll="+llString+"&radius="+radius
+
+headers = {
+    "Accept": "application/json",
+    "Authorization": get_key()
+}
+
+response = requests.request("GET", url, headers=headers)
+
+with open('hotSpots.json', 'w') as file: 
+    file.truncate(0)        # Clears file before writing to it
+    file.write(response.text)
+
