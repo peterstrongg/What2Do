e8461abcf4e499baf90d0261cbff371380b001d3
Peter Strong <peterstrong8343@gmail.com>
2021-11-24 19:53:50 -0500

    Update

--- server.py
+++ server.py
@@ -1,18 +1,24 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app = Flask(__name__)
 
-@app.route('/')
+zipCode = ''
+
+@app.route('/', methods = ['POST', 'GET'])
 def home():
+    if request.method == "POST":
+        global zipCode 
+        zipCode = str(request.form["zip"])
+        return redirect(url_for("results"))
     return render_template('index.html')
 
 @app.route('/results/')
 def results():
-    print('Results page loading...')
-    os.system('python getLocations.py 18031')
+    print('Results page loading for zipcode '+ zipCode +'...')
+    os.system('python getLocations.py ' + zipCode)
     return render_template('results.html')
     
-
+    
 if __name__ == '__main__':
     app.run(debug = True)
