449407cd52d87b3ade729eb0294e7174cc13a918
Peter Strong <peterstrong8343@gmail.com>
2021-11-25 10:27:24 -0500

    Update

--- server.py
+++ server.py
@@ -4,19 +4,25 @@ import os
 app = Flask(__name__)
 
 zipCode = ''
+category = ''
 
 @app.route('/', methods = ['POST', 'GET'])
 def home():
     if request.method == "POST":
         global zipCode 
+        global category
+
         zipCode = str(request.form["zip"])
+        category = str(request.form["selectCategory"]).lower()
+        
         return redirect(url_for("results"))
+
     return render_template('index.html')
 
 @app.route('/results/')
 def results():
-    print('Results page loading for zipcode '+ zipCode +'...')
-    os.system('python getLocations.py ' + zipCode)
+    print('Results page loading for '+ category +'\'s in zipcode '+ zipCode +'...')
+    os.system('python getLocations.py ' + zipCode + ' ' + category)
     return render_template('results.html')
     
     
