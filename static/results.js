5406188369fd81b2bdc5a094a3c0e6fd12550c1f
Peter Strong <peterstrong8343@gmail.com>
2021-11-25 10:27:39 -0500

    Update

--- static/results.js
+++ static/results.js
@@ -8,8 +8,12 @@ var jsonFile = JSON.parse(req.responseText);
 
 // Prints list of places
 var numResults = jsonFile.results.length;
-var listItem = document.getElementById("placeList");
-for(let i = 0; i < numResults; i++){
-     listItem.innerHTML += "<li>" + jsonFile.results[i].name + "</li>";
-}
 
+if(numResults >= 1){
+     var listItem = document.getElementById("placeList");
+     for(let i = 0; i < numResults; i++){
+          listItem.innerHTML += "<li>" + jsonFile.results[i].name + "</li>";
+}
+} else {
+     document.getElementById("nothingFound").innerHTML = "Nothing found under that zip code";
+}
